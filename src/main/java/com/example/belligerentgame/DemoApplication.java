d08d733d70ef82c20a748d80cb2980fa563e98a2
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2022-09-08 14:06:41 -0500

    Add a simple call using the webclient

--- src/main/java/com/example/belligerentgame/DemoApplication.java
+++ src/main/java/com/example/belligerentgame/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.belligerentgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("Performing the call");
+		WebClient client = WebClient.builder().build();
+		client.get()
+				.uri("https://postman-echo.com/get")
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
 	}
 
 }
